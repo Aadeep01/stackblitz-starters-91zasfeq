95ea8ca9e3f068f24497bd3567707294c0c4642a
Aadeep Kaur <aadeepkaur@yahoo.com>
2025-03-18 10:59:05 +0530

    updated

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const app = express();
-app.use(bodyParser.json());
+const booksRoutes = require('./routes/server'); 
 
-const PORT = process.env.PORT || 3000;
+app.use(express.json()); 
+
+app.use('/books', booksRoutes); 
+
+app.get('/', (req, res) => {
+    res.send('Library Management API is running!');
+});
+
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
